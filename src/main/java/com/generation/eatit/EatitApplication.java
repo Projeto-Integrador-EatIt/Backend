8003c8825b16b0533dea70c83fd796804a982426
Geovanna Duarte <geovannaduarttes@gmail.com>
2022-06-01 09:31:51 -0300

    Criar o Projeto Spring

---
+++ src/main/java/com/generation/eatit/EatitApplication.java
@@ -0,0 +1,13 @@
+package com.generation.eatit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/eatit/EatitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.eatit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
