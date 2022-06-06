10d6f96598070273d14f99f68539a083a98c1924
PatriciAlves <patricia.18_08@hotmail.com>
2022-06-06 09:42:12 -0300

    Categoria Repository

--- src/main/java/com/generation/eatit/repository/CategoriaRepository.java
+++ src/main/java/com/generation/eatit/repository/CategoriaRepository.java
@@ -1,5 +1,14 @@
 package com.generation.eatit.repository;
 
-public interface CategoriaRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.eatit.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	
+	public List<Categoria> findAllByTipoContainingIgnoreCase(String tipo);
 }
