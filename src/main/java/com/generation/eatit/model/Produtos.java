7fe218e533164e0d5292381e4f9b84a656369af9
PatriciAlves <patricia.18_08@hotmail.com>
2022-06-06 09:39:44 -0300

    Categoria Repository

--- src/main/java/com/generation/eatit/model/Produtos.java
+++ src/main/java/com/generation/eatit/model/Produtos.java
@@ -1,5 +1,6 @@
 package com.generation.eatit.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
---
+++ src/main/java/com/generation/eatit/repository/CategoriaRepository.java
@@ -0,0 +1,5 @@
+package com.generation.eatit.repository;
+
+public interface CategoriaRepository {
+
+}
