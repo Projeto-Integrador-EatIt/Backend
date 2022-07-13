588a9d653495be2b7374cdabb87bf4ce28757b2e
Edgard Henrique da Silva <98416812+edgardhenriqu@users.noreply.github.com>
2022-07-13 16:48:42 -0300

    Update BasicSecurityConfig.java

--- src/main/java/com/generation/eatit/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/eatit/security/BasicSecurityConfig.java
@@ -38,7 +38,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
 				.antMatchers("/usuarios/logar").permitAll()
 				.antMatchers("/usuarios/cadastrar").permitAll()
-				.antMatchers(HttpMethod.GET, "/categorias/*").permitAll()
+				.antMatchers(HttpMethod.GET, "/categorias").permitAll()
 				.antMatchers(HttpMethod.GET, "/produtos").permitAll()
 				.antMatchers(HttpMethod.GET, "/usuarios/*").permitAll()
 				.antMatchers(HttpMethod.PUT, "/usuarios/atualizar").permitAll()
