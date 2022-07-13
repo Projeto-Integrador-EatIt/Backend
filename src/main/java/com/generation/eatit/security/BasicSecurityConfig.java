39a7db589b374ce7e5bea2c7fdb67b081c1bc28f
Renan Oliveira <89881203+RenanJar@users.noreply.github.com>
2022-07-13 19:02:10 -0300

    Update BasicSecurityConfig.java

--- src/main/java/com/generation/eatit/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/eatit/security/BasicSecurityConfig.java
@@ -35,7 +35,6 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-
 				.antMatchers("/usuarios/logar").permitAll()
 				.antMatchers("/usuarios/cadastrar").permitAll()
 				.antMatchers(HttpMethod.GET, "/categorias").permitAll()
