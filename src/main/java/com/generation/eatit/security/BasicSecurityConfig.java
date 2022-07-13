066a48d8d567c427812e7ea099f8211b17cf0099
Renan Oliveira <89881203+RenanJar@users.noreply.github.com>
2022-07-13 16:23:09 -0300

    Update BasicSecurityConfig.java

--- src/main/java/com/generation/eatit/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/eatit/security/BasicSecurityConfig.java
@@ -38,6 +38,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
 				.antMatchers("/usuarios/logar").permitAll()
 				.antMatchers("/usuarios/cadastrar").permitAll()
+				.antMatchers(HttpMethod.GET, "/categorias").permitAll()
 				.antMatchers(HttpMethod.GET, "/categorias/*").permitAll()
 				.antMatchers(HttpMethod.GET, "/produtos").permitAll()
 				.antMatchers(HttpMethod.GET, "/usuarios/*").permitAll()
